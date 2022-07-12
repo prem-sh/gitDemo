3f902e338425fb9e51c9ad189ce1da132e477a90
Premkumar <premkumaralaguraj@gmail.com>
2022-07-12 15:19:26 +0530

    re-initialized git

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+
+
+
+
+//   Then merge Master and Development branch.
+//   Also create .gitignore file and add some file in gitignore.
